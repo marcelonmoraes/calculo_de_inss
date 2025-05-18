a17aff34508855ca26398a2d6aebaf48cf415669
Marcelo Moraes <marcelonmoraes@gmail.com>
2025-05-18 13:46:19 -0300

    Offenses do rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
-  
+
   context "when email is not unique" do
     let(:user) { create(:user) }
     let(:user2) { build(:user, email: user.email) }
 
     it "is not valid" do
-      expect(user2).not_to be_valid 
+      expect(user2).not_to be_valid
     end
   end
 end
