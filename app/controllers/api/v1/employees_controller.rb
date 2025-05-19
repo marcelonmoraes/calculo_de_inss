89df19e60fa67009db41670a3ccb9f07911123fc
Marcelo Moraes <marcelonmoraes@gmail.com>
2025-05-18 22:56:45 -0300

    Ajuste de offenses do rubocop

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class EmployeesController < ApplicationController
-      skip_before_action :verify_authenticity_token, only: [:calculate_discount]
+      skip_before_action :verify_authenticity_token, only: [ :calculate_discount ]
 
       def calculate_discount
         salary = params[:salary].to_f
