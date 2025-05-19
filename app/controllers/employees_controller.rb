b7f5afea1fecc9e3de5b4a1c345f9a7202afa56d
Marcelo Moraes <marcelonmoraes@gmail.com>
2025-05-18 22:48:23 -0300

    Inclusão do campo salary_discount

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,25 +1,20 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: %i[ show edit update destroy ]
 
-  # GET /employees or /employees.json
   def index
     @employees = Employee.all
   end
 
-  # GET /employees/1 or /employees/1.json
   def show
   end
 
-  # GET /employees/new
   def new
     @employee = Employee.new
   end
 
-  # GET /employees/1/edit
   def edit
   end
 
-  # POST /employees or /employees.json
   def create
     @employee = Employee.new(employee_params)
 
@@ -34,7 +29,6 @@ class EmployeesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /employees/1 or /employees/1.json
   def update
     respond_to do |format|
       if @employee.update(employee_params)
@@ -47,7 +41,6 @@ class EmployeesController < ApplicationController
     end
   end
 
-  # DELETE /employees/1 or /employees/1.json
   def destroy
     @employee.destroy!
 
@@ -58,13 +51,11 @@ class EmployeesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_employee
       @employee = Employee.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def employee_params
-      params.expect(employee: [ :name, :birth_date, :salary ])
+      params.expect(employee: [ :name, :birth_date, :salary, :salary_discount ])
     end
 end
