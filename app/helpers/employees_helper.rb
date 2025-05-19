133d5f4133e5536864434846bdd38182918d0309
Marcelo Moraes <marcelonmoraes@gmail.com>
2025-05-18 23:15:16 -0300

    Ajuste no badge das faixas

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,9 +1,9 @@
 module EmployeesHelper
   def format_salary_group(salary)
-    return content_tag(:span, "1ª Faixa", class: "badge bg-success") if salary <= CalcularContribuicaoMensal::TAX_RANGES[0][:max]
-    return content_tag(:span, "2ª Faixa", class: "badge bg-info") if salary <= CalcularContribuicaoMensal::TAX_RANGES[1][:max]
-    return content_tag(:span, "3ª Faixa", class: "badge bg-warning") if salary <= CalcularContribuicaoMensal::TAX_RANGES[2][:max]
+    return content_tag(:span, "1ª Faixa", class: "badge rounded-pill bg-success bg-gradient") if salary <= CalcularContribuicaoMensal::TAX_RANGES[0][:max]
+    return content_tag(:span, "2ª Faixa", class: "badge rounded-pill bg-light bg-gradient text-dark") if salary <= CalcularContribuicaoMensal::TAX_RANGES[1][:max]
+    return content_tag(:span, "3ª Faixa", class: "badge rounded-pill bg-warning bg-gradient") if salary <= CalcularContribuicaoMensal::TAX_RANGES[2][:max]
 
-    content_tag(:span, "4ª Faixa", class: "badge bg-danger")
+    content_tag(:span, "4ª Faixa", class: "badge rounded-pill bg-danger bg-gradient")
   end
 end
