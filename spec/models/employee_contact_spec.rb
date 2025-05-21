320e3193e2cde424a4da2eda3ddabe40d0adec30
Marcelo Moraes <marcelonmoraes@gmail.com>
2025-05-21 00:00:32 -0300

    Teste de EmployeeContact

--- spec/models/employee_contact_spec.rb
+++ spec/models/employee_contact_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe EmployeeContact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:employee) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:contact_type) }
+    it { should validate_presence_of(:value) }
+  end
 end
